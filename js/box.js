f113d5bbcf69c2286d98cfa7bde1e643f7e875f5
Austin Hallock <austinhallock@gmail.com>
2014-08-21 12:25:11 -0700

    Initial updates for HTML5DevConf (completely new presentation)

--- js/box.js
+++ js/box.js
@@ -1,13 +1,13 @@
 // Any boxes on the screen that can be walked on
 var Box = ( function() {
-	
+
 	// Text goes inside the box, this is just like your standard css padding...
 	Box.PADDING = 15;
-	
+
 	function Box( id, x, y, width, height )
 	{
 		this.id = id;
-		
+
 		// See if there's a DOM element for this id, and grab it's title tag for the box text
 		var ele = document.getElementById( id );
 		if( ele )
@@ -19,27 +19,27 @@ var Box = ( function() {
 			var size = Game.ctx.measureText( this.text );
 			this.width = size.width + Box.PADDING * 2;
 		}
-		
+
 		this.x = x;
 		this.y = y;
 		this.width = this.width || width;
 		this.height = height;
-		
+
 		// Corners
 		this.left = x;
 		this.right = x + this.width;
 		this.top = y;
 		this.bottom = y + this.height;
-		
+
 		// Background color of box
 		this.background = 'rgba( 100, 100, 100, 0.8 )';
 	}
-	
+
 	/**
 	 * Checks if this box collides with the player
 	 * @param {int} x - the player's future x position
 	 * @param {int} y - the player's future y position
-	 * @return {truthy} - the string side if a collision, false otherwise 
+	 * @return {truthy} - the string side if a collision, false otherwise
 	 */
 	Box.prototype.checkCollidesWithPlayer = function( x, y ) {
 		var playerLeft = x;
@@ -51,29 +51,29 @@ var Box = ( function() {
 		( ( playerBottom > this.top && playerBottom < this.bottom ) || ( playerTop > this.top && playerTop < this.bottom ) ) )
 			return 'left';
 		// Collision with the right side of the box
-		else if( playerRight >= this.right && playerLeft <= this.right && Player.velocity.x < 0 && 
+		else if( playerRight >= this.right && playerLeft <= this.right && Player.velocity.x < 0 &&
 		( ( playerBottom > this.top && playerBottom < this.bottom ) || ( playerTop > this.top && playerTop < this.bottom ) ) )
 			return 'right';
 		// Collision with the top side of the box
-		else if( playerBottom >= this.top && playerTop <= this.top && Player.velocity.y >= 0 && 
+		else if( playerBottom >= this.top && playerTop <= this.top && Player.velocity.y >= 0 &&
 		( ( playerRight > this.left && playerRight < this.right ) || ( playerLeft > this.left && playerLeft < this.right ) ) )
 			return 'top';
 		// Collision with the top side of the box
-		else if( playerBottom >= this.bottom && playerTop <= this.bottom && Player.velocity.y >= 0 && 
+		else if( playerBottom >= this.bottom && playerTop <= this.bottom && Player.velocity.y >= 0 &&
 		( ( playerRight > this.left && playerRight < this.right ) || ( playerLeft > this.left && playerLeft < this.right ) ) )
 			return 'bottom';
-			
+
 		// No collision
 		return false;
 	};
-	
+
 	/**
-	 * Draws the box onto the game canvas 
+	 * Draws the box onto the game canvas
 	 */
 	Box.prototype.draw = function() {
 		Game.ctx.fillStyle = this.background;
 		Game.ctx.fillRect( this.x - Game.xOffset, this.y, this.width, this.height );
-		
+
 		if( typeof this.text !== 'undefined' )
 		{
 			var fontSize = 12 * Game.SCALE.y;
@@ -81,8 +81,8 @@ var Box = ( function() {
 			Game.ctx.fillStyle = '#fff';
 			Game.ctx.fillText( this.text, this.left - Game.xOffset + Box.PADDING, this.top + fontSize + Box.PADDING );
 		}
-	}
-	
+	};
+
 	return Box;
-	
+
 } )();
--- js/player.js
+++ js/player.js
@@ -2,46 +2,46 @@ var Player = {
 	state: 'stand', // for animations
 	landed: false,
 	jumping: false,
-	
+
 	// For the walking animation
 	walkStance: 1, // walk stage 1 or 2 if walking
 	walkStanceChanged: 0,
-	
+
 	width: Math.round( 100 * spriteSheet.width / 6 ) / 100,
 	// How wide we want to make the collision object
 	// We have to do this because the sprite actually has some extra space around it...
 	collideWidth: 0.6 * Math.round( 100 * spriteSheet.width / 6 ) / 100,
 	height: Math.round( 100 * spriteSheet.height / 3 ) / 100,
-	
+
 	// If the player collides with an object, we store information about it here (and reuse this object)
 	// The reason we reuse this object is so we're not creating more unused memory that the garbage collector has to clean
 	collision: {},
 	previousCollision: {},
-	
+
 	// Starting positions
 	x: 20,
 	y: 0,
-	
+
 	// Starting velocities
 	velocity: {
 		x: 0,
 		y: 0
 	},
-	
+
 	// Velocity when the left/right keys are down
 	WALK_VELOCITY: 20,
 	// Velocity when the jump key is down
 	JUMP_VELOCITY: -30,
-	
+
 	acceleration: {
 		x: 0,
 		y: Game.GRAVITY
 	},
-	
+
 	start: function() {
 		// Don't really need to do anything, but put stuff related to the player on the game start here..
 	},
-	
+
 	getState: function() {
 		if( this.bouncing && this.velocity.x > 0 )
 			this.state = 'jumpRight';
@@ -53,12 +53,12 @@ var Player = {
 			this.state = 'fallRight';
 		else if( this.velocity.x < 0 && this.velocity.y > 0 )
 			this.state = 'fallLeft';
-			
+
 		else if( this.velocity.x > 0 && this.velocity.y < 0 )
 			this.state = 'upRight';
 		else if( this.velocity.x < 0 && this.velocity.y < 0 )
 			this.state = 'upLeft';
-			
+
 		else if( this.velocity.x > 0 )
 		{
 			if( Game.lastTime - this.walkStanceChanged > 200 )
@@ -77,24 +77,24 @@ var Player = {
 			}
 			this.state = 'walkLeft' + this.walkStance;
 		}
-		
+
 		else if( this.velocity.y < 0 )
 			this.state = 'up';
 		else if( this.velocity.y > 0 )
 			this.state = 'fall';
-		
+
 		else
 			this.state = 'stand';
 	},
-	
+
 	/**
 	 * Checks if the player collides with any objects (boxes) we have
 	 * If the is a collision, we return true and information about
 	 * the collision is stored in this.
-	 * otherwise, we return false 
+	 * otherwise, we return false
 	 * @param x - the player's future x position
 	 * @param y - the palyer's future y position
-	 * 
+	 *
 	 * If you want to get more advanced, you would calculate if the player collided between the previous position and the current one
 	 */
 	checkCollisions: function( x, y ) {
@@ -115,25 +115,25 @@ var Player = {
 					// To make sure the associated action with the collision with this object is called just once
 					this.collision.called = false;
 				}
-				
+
 				// What side of the box we collided with
 				this.collision.side = collisionSide;
 				// The box that was collided with
 				this.collision.object = Game.boxes[i];
-				
+
 				return true;
 			}
 		}
 		return false;
 	},
-	
+
 	/**
 	 * Called every time in the game loop (~16ms), moves the player around
-	 * @param {int} dt - the number of ms since the last call 
+	 * @param {int} dt - the number of ms since the last call
 	 */
 	move: function( dt ) {
 		dt /= 100;
-				
+
 		// Left / right movement
 		if( Keys.right && !this.bouncing )
 			this.velocity.x = this.WALK_VELOCITY * Game.SCALE.x;
@@ -141,7 +141,7 @@ var Player = {
 			this.velocity.x = -1 * this.WALK_VELOCITY * Game.SCALE.x;
 		else
 			this.velocity.x = 0;
-			
+
 		// Jump
 		if( Keys.up && ( this.landed || this.bouncing ) )
 		{
@@ -153,17 +153,17 @@ var Player = {
 				_this.velocity.y = _this.JUMP_VELOCITY * Game.SCALE.y;
 				_this.bouncing = false;
 				_this.landed = false;
-			}, 100 )
+			}, 100 );
 		}
-			
+
 		this.getState();
-		
-		
+
+
 		// See if the player collided with any of the objects
 		var newX = this.x + this.velocity.x * dt; // Where we want to move the player (x direction)
 		var newY = this.y + this.velocity.y * dt; // Where we want to move the player (y direction)
 		var collision = this.checkCollisions( newX, newY);
-		
+
 		// Horizontal movement
 		// hitting the left side of the canvas
 		if( newX + this.width <= 0 )
@@ -190,12 +190,12 @@ var Player = {
 		{
 			this.x = newX;
 			// Update where the 'camera' is looking (only once he hits the halfway point)
-			var newXOffset = newxOffset = this.x - ( window.innerWidth / 2 ) > 0;
+			var newXOffset = this.x - ( window.innerWidth / 2 ) > 0;
 			if( newXOffset )
 				Game.xOffset = this.x - ( window.innerWidth / 2 );
 		}
-			
-		
+
+
 		// Vertical movement
 		// hitting top of page & going up
 		if( this.top <= 0 && this.velocity.y < 0 )
@@ -205,7 +205,7 @@ var Player = {
 		}
 		// if still 'falling', but outside of bounds of page OR the player collides with an object's top
 		else if( this.bottom >= Game.canvas.height && this.velocity.y >= 0 )
-		{				
+		{
 			this.velocity.y = 0;
 			this.y = Game.canvas.height - this.height; // bottom of page
 			this.landed = true;
@@ -214,7 +214,7 @@ var Player = {
 		{
 			this.velocity.y = 0;
 			this.y = this.collision.object.top - this.height;
-			
+
 			// For the purpose of this game, when a box is collided with, we switch to that slide
 			var landedElement = document.getElementById( this.collision.object.id );
 			if( landedElement )
@@ -234,7 +234,7 @@ var Player = {
 					landedElement.className = 'slide show';
 				}
 			}
-				
+
 			this.landed = true;
 		}
 		else // below top of page, above bottom, or on the ground and jumping
@@ -245,25 +245,25 @@ var Player = {
 			this.velocity.y += this.acceleration.y * Game.SCALE.y * dt;
 			if( this.velocity.y > Game.MAX_VELOCITY )
 				this.velocity.y = Game.MAX_VELOCITY;
-			
+
 			this.y = newY;
 		}
-		
+
 		// Bounding box stuff
-		this.top = this.y
+		this.top = this.y;
 		this.bottom = this.height + this.top;
 		this.left = this.x;
 		this.right = this.x + this.collideWidth;
 	},
-	
+
 	/**
-	 * Draw the player sprite on the canvas 
+	 * Draw the player sprite on the canvas
 	 */
 	draw: function() {
 		var sprite = sprites[ this.state ];
-		
+
 		// Actually draw the player sprite on the canvas
-		// Parameters are: img, sx (x coordinate for clipping), sy (y coordinate for clipping), swidth (width of clipped image), sheight (height of clipped image), x, y, width, height		
+		// Parameters are: img, sx (x coordinate for clipping), sy (y coordinate for clipping), swidth (width of clipped image), sheight (height of clipped image), x, y, width, height
 		Game.ctx.drawImage( spriteSheet, sprite.x, sprite.y, this.width, this.height, this.x - Game.xOffset, this.y, this.width, this.height );
 	}
 };
